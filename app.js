840596db6270efc07b4ccf7de39f4419878d3c0b
Santha-Ashwin-I-A <santhaashwinwebdev@gmail.com>
2024-10-11 08:07:15 +0530

    second commit

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
 const express = require('express');
+
 
 const app = express();
 
 app.get('', (req, res) => {
-    res.send('Welcome repo');    
+    res.send('Welcome repo abc');    
 });
 
 app.listen(4000, () => {
